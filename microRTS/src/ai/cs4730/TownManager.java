b3b5333b9b7750ab7cafe1d49b84a75c07597d6c
tjharrison <tjharrison.jr@gmail.com>
2013-06-08 19:20:20 -0700

    added some convenient helper methods for dealing with resources

--- microRTS/src/ai/cs4730/TownManager.java
+++ microRTS/src/ai/cs4730/TownManager.java
@@ -56,14 +56,7 @@ public class TownManager extends Manager{
 
          if(ai.workers.size() + ai.scouts.size() < ai.wantedScouts + ai.wantedWorkers){// do we want scouts?
             // check if we have the cash
-            boolean enoughMoney = true;
-            for(int i = 0; i < ai.resources.size(); i++){
-               int cost = ai.unitTypes.get(AIController.WORKER).cost.get(i);
-               if(cost > ai.resources.get(i)){
-                  enoughMoney = false;
-                  break;
-               }
-            }
+            boolean enoughMoney = canAffordUnit(AIController.WORKER);
             if(enoughMoney){
                if(stock.actions.size() <= 0){ // no actions?!?!?
                   int time = ai.currentTurn;
@@ -73,16 +66,40 @@ public class TownManager extends Manager{
                   for(int i = 0; i < ai.unitTypes.get(AIController.WORKER).produce_speed - 1; i++){
                      stock.addAction(new UnitAction(stock.unit, UnitAction.NONE, stock.getX(), stock.getY(), -1), MapUtil.trafficMap, position, time, time + 1);
                   }
-                  for(int i = 0; i < ai.unitTypes.get(AIController.WORKER).cost.size(); i++){
-                     int newAmount = ai.resources.get(i) - ai.unitTypes.get(AIController.WORKER).cost.get(i);
-                     ai.resources.set(i, newAmount);
-                  }
+                  makePurchase(ai.unitTypes.get(AIController.WORKER).cost);
                   if(AIController.DEBUG){
                      System.out.println("TM: recruiting worker");
                   }
                }
             }
          }
+      }
+   }
+   
+   public boolean canAffordUnit(int type){
+      for(int i = 0; i < ai.resources.size(); i++){
+         int cost = ai.unitTypes.get(type).cost.get(i);
+         if(cost > ai.resources.get(i)){
+            return false;
+         }
+      }
+      return true;
+   }
+   
+   public boolean canAffordBuilding(int type){
+      for(int i = 0; i < ai.resources.size(); i++){
+         int cost = ai.buildingTypes.get(type).cost.get(i);
+         if(cost > ai.resources.get(i)){
+            return false;
+         }
+      }
+      return true;
+   }
+   
+   public void makePurchase(ArrayList<Integer> cost){
+      for(int i = 0; i < cost.size(); i++){
+         int newAmount = ai.resources.get(i) - cost.get(i);
+         ai.resources.set(i, newAmount);
       }
    }
 
